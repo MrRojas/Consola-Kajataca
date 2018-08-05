9d58c8c1602f82c95212e1ea5576220e3556d761
= <=>
2018-08-05 10:13:02 -0400

    control para todas las acciones del CLI kajataca

---
+++ clases/Menu.py
@@ -0,0 +1,19 @@
+from clases.Pantalla import *
+
+class Menu(Pantalla):
+	
+	def __init__(self):
+		self.fondo('4f')
+		self.cabezera('Kajataca')
+		
+		pass 
+
+	def principal(self):
+
+		self.titulo(" CLI Kajataca 1.0...")
+		self.comando()
+
+	
+
+
+		
